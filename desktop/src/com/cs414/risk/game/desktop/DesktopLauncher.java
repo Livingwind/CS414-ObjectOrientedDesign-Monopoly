2d7f77221a2b450fcc61d0a75c326c33514e03d3
Livingwind <westerman.christopher@gmail.com>
2018-03-13 19:50:05 -0600

    I don't like gradle but here is a starting place for the libgdx engine.
    Most of this setup was auto-created by a script provided by libGDX referenced by https://libgdx.badlogicgames.com/download.html and running the gradle setup application.

---
+++ desktop/src/com/cs414/risk/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cs414.risk.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cs414.risk.game.Risk;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Risk(), config);
+	}
+}
