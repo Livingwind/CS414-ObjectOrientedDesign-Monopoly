3479c4411982de96e7ae7899e11f99bf9d4c48a9
Livingwind <westerman.christopher@gmail.com>
2018-04-13 23:19:22 -0600

    A dialog needed to be displayed when a property was landed on.
    Made a dialog window that presents the player with a choice to buy or
    auction the property. If the space has been landed on, the player gets
    charged rent.

--- core/src/main/java/com/cs414/monopoly/spaces/PropertySpace.java
+++ core/src/main/java/com/cs414/monopoly/spaces/PropertySpace.java
@@ -3,6 +3,7 @@ package com.cs414.monopoly.spaces;
 import com.badlogic.gdx.utils.JsonValue;
 import com.cs414.monopoly.entities.Player;
 import com.cs414.monopoly.entities.Property;
+import com.cs414.monopoly.ui.PropertyLandDialog;
 
 public abstract class PropertySpace extends AbstractSpace {
   Property property;
@@ -11,20 +12,8 @@ public abstract class PropertySpace extends AbstractSpace {
     super(filename, location, props, Size.STANDARD);
   }
 
-  void buyDialog(Player player) {
-    // TODO Create a dialog for the player to purchase the property
-    // TODO If no is selected, move to auction.
-  }
-
   @Override
   public void onLand(Player player) {
-    if(property.ownedBy != null) {
-      if(!property.mortgaged) {
-        player.modifyMoney(-property.getRent());
-        return;
-      }
-    }
-
-    buyDialog(player);
+    new PropertyLandDialog(property);
   }
 }
--- core/src/main/java/com/cs414/monopoly/ui/PropertyLandDialog.java
+++ core/src/main/java/com/cs414/monopoly/ui/PropertyLandDialog.java
@@ -1,15 +1,61 @@
 package com.cs414.monopoly.ui;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.cs414.monopoly.entities.Property;
 
 public class PropertyLandDialog extends PropertyDialog {
 
   public PropertyLandDialog(Property property) {
-    super(property);
+    super(property, false);
   }
+
+  public void ownedOptions() {
+
+  }
+
+  public void unownedOptions() {
+    String price = String.format("Buy property for $%d?", property.value);
+    text(price);
+
+    Button buyProperty = new TextButton("Buy Property", getSkin());
+    buyProperty.padRight(10).padLeft(10);
+    buyProperty.setColor(Color.GREEN);
+    buyProperty.addListener(new ChangeListener(){
+      @Override
+      public void changed(ChangeEvent event, Actor actor){
+        System.out.println("Bought");
+        remove();
+        event.cancel();
+      }
+    });
+    Button auction = new TextButton("Auction", getSkin());
+    auction.padRight(10).padLeft(10);
+    auction.setColor(Color.RED);
+    auction.addListener(new ChangeListener(){
+      @Override
+      public void changed(ChangeEvent event, Actor actor){
+        System.out.println("Sent to auction");
+        event.cancel();
+      }
+    });
+
+    getContentTable().row(); // put text under image
+    button(buyProperty);
+    button(auction);
+  }
+
 
   @Override
   public void fill() {
-
+    getTitleLabel().setText("You landed on " + property.name);
+    if(property.ownedBy != state.getCurrentPlayer()) {
+      unownedOptions();
+    } else {
+      ownedOptions();
+    }
   }
 }
