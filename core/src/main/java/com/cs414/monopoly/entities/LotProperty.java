5fcbb50bffeecefd3e72f1add3fb59e2c18ad822
Christian Meyer <koti@rams.colostate.edu>
2018-04-18 21:21:43 -0600

    Added static mapping of colors to LotProperty  (#103)
    * Added static mapping of colors to LotProperty and fixed build.gradle to use java 1.8 instead of 1.6
    
    * Refactored color map for simplicity

--- core/src/main/java/com/cs414/monopoly/entities/LotProperty.java
+++ core/src/main/java/com/cs414/monopoly/entities/LotProperty.java
@@ -1,8 +1,13 @@
 package com.cs414.monopoly.entities;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.JsonValue;
 
+import java.util.HashMap;
+
 public class LotProperty extends Property {
+  private static HashMap<Color, Integer> colorMap = new HashMap<>();
+  public final Color color;
   public int houseCost;
   private int numHouses;
   private int rentIndex;
@@ -10,6 +15,34 @@ public class LotProperty extends Property {
   public LotProperty(String filename, JsonValue props) {
     super(filename, props);
     houseCost = props.get("houseCost").asInt();
+    color = nameToColor(props.getString("color"));
+    colorMap.put(color, colorMap.getOrDefault(color, 0) + 1);
+  }
+
+  private Color nameToColor(String name) {
+    switch (name) {
+      case "brown":
+        return Color.BROWN;
+      case "cyan":
+        return Color.CYAN;
+      case "magenta":
+        return Color.MAGENTA;
+      case "orange":
+        return Color.ORANGE;
+      case "red":
+        return Color.RED;
+      case "yellow":
+        return Color.YELLOW;
+      case "green":
+        return Color.GREEN;
+      case "navy":
+        return Color.NAVY;
+    }
+    return new Color();
+  }
+
+  public int getColorNumber(LotProperty property) {
+    return colorMap.get(property.color);
   }
 
   /**
