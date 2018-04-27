cb504c01555242fce09868aaf14fbf03d0019d9d
Ian <thehumoroushorse@gmail.com>
2018-04-27 01:46:54 -0600

    fixed the money sorting in scoreboard

--- core/src/main/java/com/cs414/monopoly/ui/playerhud/PlayerMoneyComparator.java
+++ core/src/main/java/com/cs414/monopoly/ui/playerhud/PlayerMoneyComparator.java
@@ -6,6 +6,6 @@ import java.util.Comparator;
 public class PlayerMoneyComparator implements Comparator<Player> {
   @Override
   public int compare(Player p1, Player p2) {
-    return p1.getNetWorth() - p2.getNetWorth();
+    return p2.getMoney() - p1.getMoney();
   }
 }
